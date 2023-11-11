de3380a77372d76d80a2b96b95b7e48aab71f908
Read <decimation001@gmail.com>
2023-11-10 22:40:49 -0600

    Add project files.

---
+++ NLocalSend.Main/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json.Nodes;
+
+namespace NLocalSend.Main
+{
+	internal class Program
+	{
+
+		public static async Task Main(string[] args)
+		{
+			var u = new NlsUdpClient();
+			u.Join();
+			var o = await u.ReceiveResponseAsync();
+			Console.WriteLine(o);
+			HttpListener listener = new HttpListener();
+			listener.Start();
+			var cl=new HttpClient();
+			cl.SendAsync(new HttpRequestMessage(HttpMethod.Post, "POST /api/localsend/v2/register"))
+			var          c        =await listener.GetContextAsync();
+			Console.WriteLine(c);
+		}
+
+	}
+}
---
+++ NLocalSend/NlsUdpClient.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NLocalSend
+{
+	public class NlsUdpClient
+	{
+
+		public const int DEFAULT_UDP_PORT = 53317;
+
+		public static readonly IPAddress DefaultMulticastGroup = IPAddress.Parse("224.0.0.167");
+
+		/*
+		 * LocalSend does not require a specific port or multicast address but instead provides a default configuration.
+
+		   Everything can be configured in the app settings if the port / address is somehow unavailable.
+
+		   The default multicast group is 224.0.0.0/24 because some Android devices reject any other multicast group.
+
+		   Multicast (UDP)
+
+		   Port: 53317
+		   Address: 224.0.0.167
+
+		   HTTP (TCP)
+
+		   Port: 53317
+
+		 */
+
+		public UdpClient Client { get; private set; }
+
+		public NlsUdpClient(int port = DEFAULT_UDP_PORT)
+		{
+			Client = new UdpClient(port);
+		}
+
+		public void Join()
+		{
+			Client.JoinMulticastGroup(DefaultMulticastGroup);
+		}
+
+		public async Task<NlsResponse> ReceiveResponseAsync()
+		{
+			var res = await Client.ReceiveAsync();
+			var o = JsonSerializer.Deserialize<NlsResponse>(res.Buffer);
+			o.Result = res;
+			return o;
+		}
+
+	}
+
+	// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+	public class NlsResponse
+	{
+		[JsonIgnore]
+		public UdpReceiveResult Result { get; internal set; }
+
+		[JsonPropertyName("alias")]
+		public string Alias { get; set; }
+
+		[JsonPropertyName("version")]
+		public string Version { get; set; }
+
+		[JsonPropertyName("deviceModel")]
+		public string DeviceModel { get; set; }
+
+		[JsonPropertyName("deviceType")]
+		public string DeviceType { get; set; }
+
+		[JsonPropertyName("fingerprint")]
+		public string Fingerprint { get; set; }
+
+		[JsonPropertyName("port")]
+		public int Port { get; set; }
+
+		[JsonPropertyName("protocol")]
+		public string Protocol { get; set; }
+
+		[JsonPropertyName("download")]
+		public bool Download { get; set; }
+
+		[JsonPropertyName("announcement")]
+		public bool Announcement { get; set; }
+
+		[JsonPropertyName("announce")]
+		public bool Announce { get; set; }
+
+		public override string ToString()
+		{
+			return
+				$"{nameof(Alias)}: {Alias}, {nameof(Version)}: {Version}, {nameof(DeviceModel)}: {DeviceModel}, " +
+				$"{nameof(DeviceType)}: {DeviceType}, {nameof(Fingerprint)}: {Fingerprint}, {nameof(Port)}: {Port}, " +
+				$"{nameof(Protocol)}: {Protocol}, {nameof(Download)}: {Download}, {nameof(Announcement)}: {Announcement}, " +
+				$"{nameof(Announce)}: {Announce}";
+		}
+
+	}
+}
